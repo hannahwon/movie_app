baea1b487e07b06327d03302b87272a80c96799e
Hannah <shw9401@naver.com>
2021-10-18 23:15:55 +0900

    6.4 Finished movie.app

--- src/components/Movie.js
+++ src/components/Movie.js
@@ -1,26 +1,39 @@
 import React from "react";
+import { Link } from "react-router-dom";
 import PropTypes from "prop-types";
 import "../components/Movie.css";
 
 function Movie({ id, year, title, summary, poster, genres }) {
     return (
         <div className="movie">
-            <img src={poster} alt={title} title={title} />
-            <div classclassName="movie__data">
-                <h3 classclassName="movie__title">
-                    {title}
-                </h3>
-                <h5 clasclassNames="movie__year">{year}</h5>
-                <ul className="movie__genres">
-                    {genres.map((genre, index) => (
-                        <li key={index} className="genres__genre" >
-                            {genre}
-                        </li>
-                    ))}
-                </ul>
-                <p classclassName="movie__summary">{summary.slice(0, 180)}...</p>
-            </div>
-        </div>
+            <Link to={{
+                pathname: `movie/${id}`,
+                state: {
+                    year,
+                    title,
+                    summary,
+                    poster,
+                    genres
+                }
+            }}
+            >
+                <img src={poster} alt={title} title={title} />
+                <div classclassName="movie__data">
+                    <h3 classclassName="movie__title">
+                        {title}
+                    </h3>
+                    <h5 clasclassNames="movie__year">{year}</h5>
+                    <ul className="movie__genres">
+                        {genres.map((genre, index) => (
+                            <li key={index} className="genres__genre" >
+                                {genre}
+                            </li>
+                        ))}
+                    </ul>
+                    <p classclassName="movie__summary">{summary.slice(0, 180)}...</p>
+                </div>
+            </Link >
+        </div >
     );
 }
 
